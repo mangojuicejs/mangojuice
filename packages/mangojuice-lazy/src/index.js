3691e2fb7b5e5ad8169580d7756c9f32a6e9c9f3
Artem Artemyev <viiv.c58@gmail.com>
2017-07-03 23:11:38 +0500

    small additions to publish process

--- packages/mangojuice-lazy/src/index.js
+++ packages/mangojuice-lazy/src/index.js
@@ -1,5 +1,4 @@
 import { createLazyBlock } from "./createLazyBlock";
 import { getLazyCommand } from "./getLazyCommand";
 
-export { createLazyBlock, getLazyCommand };
 export default { createLazyBlock, getLazyCommand };
