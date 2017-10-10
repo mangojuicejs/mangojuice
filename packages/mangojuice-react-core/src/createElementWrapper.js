ed3b2680db4b10c339817c85254a6aaabeafaa22
Artem Artemyev <viiv.c58@gmail.com>
2017-10-11 04:56:01 +0500

    feat(react): added statefull components patching to support logic

--- packages/mangojuice-react-core/src/createElementWrapper.js
+++ packages/mangojuice-react-core/src/createElementWrapper.js
@@ -1,5 +1,17 @@
-import { getContext } from "./ViewRenderContext";
+import { getContext, setContext } from "./ViewRenderContext";
 
+
+// Replace some function in prototype and call
+// the original after replaced one if defined
+const extendPrototypeWithSuper = (proto, name, func) => {
+  const oldFunc = proto[name];
+  proto[name] = function(...args) {
+    func.apply(this, args);
+    if (oldFunc) {
+      return oldFunc.apply(this, args);
+    }
+  }
+};
 
 export default (reactImpl) => {
   // Check if already wrapped
@@ -7,10 +19,36 @@ export default (reactImpl) => {
     return reactImpl.createElement;
   }
 
+  // Extend statefull component by adding extra lifecycle
+  // methods to track context while render function
+  const injectContextToStatefull = (View) => {
+    // Do nothing if context injected pr not statefull
+    if (
+      View.__contextInjected ||
+      !(View.prototype instanceof reactImpl.Component)
+    ) {
+      return;
+    }
+    extendPrototypeWithSuper(View.prototype, 'componentWillMount', function() {
+      this.__appContext = getContext();
+    });
+    extendPrototypeWithSuper(View.prototype, 'componentWillUpdate', function() {
+      this.__oldContext = getContext();
+      setContext(this.__appContext);
+    });
+    extendPrototypeWithSuper(View.prototype, 'componentDidUpdate', function() {
+      setContext(this.__oldContext);
+      this.__oldContext = null;
+    });
+    View.__contextInjected = true;
+  };
+
   // Patching createElement fuction to support
   // commands and command creators as a prop
   const wrappedCreateElement = function(View, props, ...args) {
     const context = getContext();
+    injectContextToStatefull(View);
+
     if (props && context) {
       // Convert commands to handler functions, which will
       // execute command in current context
