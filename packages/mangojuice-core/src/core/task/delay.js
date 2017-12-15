e7ad164257572f5fdabee3f9e876652967e63ab0
Artem Artemyev <viiv.c58@gmail.com>
2017-12-16 01:26:24 +0500

    feat(core): provide a way to limit delay with any amount of milliseconds

--- packages/mangojuice-core/src/core/task/delay.js
+++ packages/mangojuice-core/src/core/task/delay.js
@@ -1,18 +1,32 @@
 import { CANCEL } from './callTask';
 
+
+// Constants
+let DELAY_LIMIT = Infinity;
+
 /**
  * Wait some time. Support cancellation in the `call`.
  * @param  {number}  ms
- * @param  {any} val
  * @return {Promise}
  */
-export function delay(ms, val = true) {
+export function delay(ms) {
+  const actualMs = ms > DELAY_LIMIT ? DELAY_LIMIT : ms;
   let timeoutId;
   const res = new Promise(resolve => {
-    timeoutId = setTimeout(() => resolve(val), ms);
+    timeoutId = setTimeout(() => resolve(), actualMs);
   });
   res[CANCEL] = () => clearTimeout(timeoutId);
   return res;
 }
+
+/**
+ * Set upper delay limit so any delay will take no longer
+ * than given amount of milliseconds. Useful for testing
+ * and server rendering.
+ * @param  {number} ms
+ */
+delay.setLimit = (ms) => {
+  DELAY_LIMIT = ms;
+};
 
 export default delay;
